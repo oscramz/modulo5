fe604a5715590508169eed37fccf4cc6c4e906db
oscramz <oscram@gmail.com>
2021-08-28 12:48:07 -0400

    Agregando prompt y alert selector de cine

--- JS/ejercicios.js
+++ JS/ejercicios.js
@@ -44,15 +44,15 @@ let total = "5 + 3 + 7 = "
 
 console.log(total + aa + bb + cc)
 
-//Ejercicio 6
-let usuario1 = { nombre: "juan", apellido: "perez" }
-let usuario2 = { nombre: "sara", apellido: "aparicio" }
-let usuario3 = { nombre: "sebastián", apellido: "gonzales" }
-let usuario4 = { nombre: "catalina", apellido: "rodriguez" }
-let usuario5 = { nombre: "laura", apellido: "quintanilla" }
-let usuario6 = { nombre: "camila", apellido: "lopez" }
-let usuario7 = { nombre: "carlos", apellido: "gutierrez" }
-let usuario8 = { nombre: "sergio", apellido: "chacon" }
+/*//Ejercicio 6
+let usuario1 = {nombre: "juan", apellido: "perez"}
+let usuario2 = {nombre: "sara", apellido: "aparicio"}
+let usuario3 = {nombre: "sebastián", apellido: "gonzales"}
+let usuario4 = {nombre: "catalina", apellido: "rodriguez"}
+let usuario5 = {nombre: "laura", apellido: "quintanilla"}
+let usuario6 = {nombre: "camila", apellido: "lopez"}
+let usuario7 = {nombre: "carlos", apellido: "gutierrez"}
+let usuario8 = {nombre: "sergio", apellido: "chacon"}
 w = 5
 
 if (w == 1) {
@@ -77,8 +77,55 @@ if (w == 1) {
 
 let usuarios = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8]
 
-if(w <= usuarios.length) {​​​
+if(w <= usuarios.length){​​​
     console.log("Hola con arreglo, " + usuarios[w-1].nombre +" "+ usuarios[w-1].apellido)
 }​​​else{​​​
 console.log("Usuario no registrado")
-}​​
+}​​*/
+
+/*//Ejercicio 7
+let j = 8
+let k = 8
+
+if (j > k){
+    console.log(j + " Es mayor que " + k)
+}else if (j < k){
+    console.log(k + " Es mayor que " + j)
+}else{
+    console.log("Los números son iguales")
+}*/
+
+//Ejercicio 8
+let q = prompt("Ingrese el número a comprobar")
+
+if (q % 2 == 0){
+    console.log(q + " Es un número par")
+}else{
+    console.log(q + " Es un número impar")
+}
+
+//Ejercicio 9
+let age = prompt("Ingrese su edad")
+let cat = prompt("Ingrese una categoria: A, B, C o D") .toUpperCase();
+
+if (age >= 0 && cat == "A" || cat == "B" || cat == "C" || cat == "D"){
+    if (cat == "A"){
+        alert("Bienvenid(a) puede ver la categoría seleccionada A")
+    }else if (cat == "B" && age >= 9){
+        alert("Bienvenid(a) puede ver la categoría seleccionada B")
+    }else if (cat == "C" && age >= 18){
+        alert("Bienvenid(a) puede ver la categoría seleccionada C")
+    }else if (cat == "D" && age >= 25){
+        alert("Bienvenid(a) puede ver la categoría seleccionada D")
+    }else{
+        alert("No puede ver la categoría seleccionada")
+    }
+}else{
+    alert("Edad o categoría invalidas")
+}
+
+/*A => Todo público
+B => Desde 9 años
+C => Desde 18 años
+D => Desde 25 años
+El usuario debe ingresar su edad y la categoría de la película que desea ver y el software deberá imprimir en consola si puede no ver la película según la clasificación.*/
