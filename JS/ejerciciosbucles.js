dac0bb890d8b48f6370fe0cc27d64a0756188a99
oscramz <oscram@gmail.com>
2021-08-28 18:02:59 -0400

    Ajuste de ejercicios 1 al 3

--- JS/ejerciciosbucles.js
+++ JS/ejerciciosbucles.js
@@ -1,5 +1,5 @@
 //Ejercicio 1
-/*let n = prompt("Ingrese el número de repeticiones")
+let n = prompt("Ingrese el número de repeticiones")
 let salida = ""
 
 for (let index = 0; index < n; index++) {
@@ -10,27 +10,27 @@ for (let index = 0; index < n; index++) {
         salida = salida + index + "-"
     }
 }
-    alert(salida)*/
+    alert(salida)
 
 //Ejercicio 2
-/*let nn = prompt("Ingrese el número de repeticiones negativas")
+let nn = prompt("Ingrese el número de repeticiones negativas")
 let salida1 = ""
 
 for (let index1 = 0; index1 <= nn; index1++) {
 
     if(index1 == nn){
-        salida1 += (index * -1) //salida = salida + index1  esta linea resume la anterior
-    }else
-        salida1 += (index * -1) + ";"
-    
+        salida1 += (index1 * -1) //salida = salida + index1  esta linea resume la anterior
+    }else{
+        salida1 += (index1 * -1) + ";"
+    }
 }
-    alert(salida1)*/
+    console.log(salida1)
 
 //Ejercicio 3
 let x = prompt("Ingrese el número de repeticiones")
-let resultado = 0
+let total = 0
 
-for (let valor = 0; valor >= x; valor++) {
-        resultado += valor
+for (let valor = 0; valor <= x; valor++) {
+        total += valor
 }
-    alert(resultado)
+    console.log(total)
