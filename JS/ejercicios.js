b81dac8fc1bf9994bbe7099c87bc804a48d3a059
oscramz <oscram@gmail.com>
2021-08-27 22:51:48 -0400

    primeros 6 ejercicios, uso del if al final

---
+++ JS/ejercicios.js
@@ -0,0 +1,76 @@
+let nombre="Oscar"
+let saludo="Bienvenido "
+
+alert( saludo+nombre )
+
+console.log(saludo + nombre)
+
+//Ejercicio 1
+let n = 90
+let m = 50
+let r1 = n + m
+
+console.log(r1)
+
+//Ejercicio 2
+let a = 25
+let b = 44
+let c = 125
+let d = 80
+let e = 37
+let r2 = a * b * c * d * e
+
+console.log(r2)
+
+//Ejercicio 3
+let x = 8
+let y = 12
+let z = 3
+let r3 = ((x + z) * y * y)
+
+alert(r3)
+
+//Ejercicio 4
+let hola = "hola "
+let mundo = "mundo"
+
+console.log(hola + mundo)
+
+//Ejercicio 5
+let aa = "5"
+let bb = "3"
+let cc = "7"
+let total = "5 + 3 + 7 = "
+
+console.log(total + aa + bb + cc)
+
+//Ejercicio 6
+let usuario1 = { nombre: "juan", apellido: "perez" }
+let usuario2 = { nombre: "sara", apellido: "aparicio" }
+let usuario3 = { nombre: "sebastián", apellido: "gonzales" }
+let usuario4 = { nombre: "catalina", apellido: "rodriguez" }
+let usuario5 = { nombre: "laura", apellido: "quintanilla" }
+let usuario6 = { nombre: "camila", apellido: "lopez" }
+let usuario7 = { nombre: "carlos", apellido: "gutierrez" }
+let usuario8 = { nombre: "sergio", apellido: "chacon" }
+i = 5
+
+if (i == 1) {
+    console.log("Hola, " + usuario1.nombre +" "+ usuario1.apellido)
+}else if(i == 2){
+    console.log("Hola, " + usuario2.nombre +" "+ usuario2.apellido)
+}else if(i == 3){
+    console.log("Hola, " + usuario3.nombre +" "+ usuario3.apellido)
+}else if(i == 4){
+    console.log("Hola, " + usuario4.nombre +" "+ usuario4.apellido)
+}else if(i == 5){
+    console.log("Hola, " + usuario5.nombre +" "+ usuario5.apellido)
+}else if(i == 6){
+    console.log("Hola, " + usuario6.nombre +" "+ usuario6.apellido)
+}else if(i == 7){
+    console.log("Hola, " + usuario7.nombre +" "+ usuario7.apellido)
+}else if(i == 8){
+    console.log("Hola, " + usuario8.nombre +" "+ usuario8.apellido)
+}else{
+    console.log("El usuario no existe")
+}
